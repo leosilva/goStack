afcc1d6ba077693e8eea377f6674144b0651fea2
Leo Moreira Silva <leosilva@iMac-de-Leo.local>
2020-04-04 18:35:04 -0300

    finalização do back-end da aplicação de exemplo

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,4 +1,6 @@
 const connection = require('../../database/connection');
+const { attachPaginate } = require('knex-paginate');
+attachPaginate();
 
 module.exports = {
     async create(request, response) {
@@ -8,7 +10,35 @@ module.exports = {
     },
 
     async list(request, response) {
-        const productList = await connection('produto').select('*');
+        const { page = 1 } = request.query;
+        const productList = await connection('produto')
+            .select('*')
+            .paginate({
+                perPage: 5,
+                currentPage: page
+            });
+            
         return response.json(productList);
+    },
+
+    async findById(request, response) {
+        const productList = await connection('produto').select('*').where({id: request.params.id});
+        return response.json(productList);
+    },
+
+    async update(request, response, next) {
+        const id = await connection('produto')
+            .where({id: request.params.id})
+            .update(request.body, ['id']);
+        
+        next();
+    },
+
+    async delete(request, response) {
+        const id = await connection('produto')
+        .where({id: request.params.id})
+        .del();
+
+        return response.send();
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,8 @@ const ProductController = require('./controllers/ProductController');
 // rotas
 routes.post('/product', ProductController.create);
 routes.get('/product', ProductController.list);
+routes.put('/product/:id', ProductController.update, ProductController.findById);
+routes.get('/product/:id', ProductController.findById);
+routes.delete('/product/:id', ProductController.delete);
 
 module.exports = routes;
